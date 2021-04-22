bf59c4a8f61f930fa47f93a3c8ed25c92ff7ada7
nikol412 <travin.nikita@gmail.com>
2021-04-22 20:36:32 +0600

    add overview fragment and viewModel

---
+++ app/src/main/java/com/nikol412/artroom/ui/OverviewFragment.kt
@@ -0,0 +1,27 @@
+package com.nikol412.artroom.ui
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.nikol412.artroom.R
+
+class OverviewFragment : Fragment() {
+
+    private lateinit var viewModel: OverviewViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_overview, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(OverviewViewModel::class.java)
+    }
+
+}
---
+++ app/src/main/java/com/nikol412/artroom/ui/OverviewViewModel.kt
@@ -0,0 +1,7 @@
+package com.nikol412.artroom.ui
+
+import androidx.lifecycle.ViewModel
+
+class OverviewViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
