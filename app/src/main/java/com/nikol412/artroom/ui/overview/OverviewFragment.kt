56529f023e631bb50884720e4be43b820f181b6e
nikol412 <travin.nikita@gmail.com>
2021-04-24 21:05:37 +0600

    added endless scrolling

--- app/src/main/java/com/nikol412/artroom/ui/overview/OverviewFragment.kt
+++ app/src/main/java/com/nikol412/artroom/ui/overview/OverviewFragment.kt
@@ -4,9 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AbsListView
+import android.widget.Button
+import android.widget.ProgressBar
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.nikol412.artroom.R
 import com.nikol412.artroom.data.ArtAPI
@@ -36,14 +41,36 @@ class OverviewFragment : Fragment() {
 
         view.findViewById<RecyclerView>(R.id.recycler_view_artworks).adapter = adapter
 
-        lifecycleScope.launch {
-            val artworksPage = withContext(Dispatchers.IO) {
-                ArtAPI.getApi().getArtsPage(1)
+        view.findViewById<RecyclerView>(R.id.recycler_view_artworks).addOnScrollListener(object: RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                if((recyclerView.layoutManager as? LinearLayoutManager)?.findLastVisibleItemPosition() == viewModel.artworksLD.value?.size?.minus(1)) {
+                    viewModel.loadNextPage()
+                }
             }
-            adapter.setItems(artworksPage.toArtworkObjectList().filter { it.imageId.isNullOrBlank().not() })
-        }
+        })
+
+        viewModel.artworksLD.observe(viewLifecycleOwner, Observer { dataList ->
+            adapter.setItems(dataList)
+        })
+        viewModel.isLoading.observe(viewLifecycleOwner, Observer { isLoading ->
+            if(isLoading) {
+                view.findViewById<ProgressBar>(R.id.progress_bar).visibility = View.VISIBLE
+            } else {
+                view.findViewById<ProgressBar>(R.id.progress_bar).visibility = View.GONE
+            }
 
 
+        })
+
+//        view.findViewById<Button>(R.id.button_load_more).setOnClickListener {
+//            viewModel.loadNextPage()
+//        }
+//        lifecycleScope.launch {
+//            val artworksPage = withContext(Dispatchers.IO) {
+//                ArtAPI.getApi().getArtsPage(1)
+//            }
+//            adapter.setItems(artworksPage.toArtworkObjectList().filter { it.imageId.isNullOrBlank().not() })
+//        }
     }
 
 }
--- app/src/main/java/com/nikol412/artroom/ui/overview/OverviewViewModel.kt
+++ app/src/main/java/com/nikol412/artroom/ui/overview/OverviewViewModel.kt
@@ -1,7 +1,49 @@
 package com.nikol412.artroom.ui.overview
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.nikol412.artroom.data.ArtAPI
+import com.nikol412.artroom.data.response.ArtworkObject
+import com.nikol412.artroom.data.response.toArtworkObjectList
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class OverviewViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val artworks = MutableLiveData(mutableListOf<ArtworkObject>())
+    val artworksLD: LiveData<MutableList<ArtworkObject>> = artworks
+
+    val isLoading = MutableLiveData(false)
+
+    var currentPage = 1
+    var totalPages = -1
+
+    init {
+        loadNextPage()
+    }
+
+    fun loadNextPage() {
+        viewModelScope.launch {
+            if (totalPages == -1 || totalPages > currentPage) {
+                isLoading.value = true
+                val artworksPage = withContext(Dispatchers.IO) {
+                    ArtAPI.getApi().getArtsPage(currentPage + 1)
+                }
+                isLoading.value = false
+
+                currentPage = artworksPage.pagination.currentPage
+                totalPages = artworksPage.pagination.totalPages
+
+                val newList = artworks.value
+                val resultFilteredList = artworksPage.toArtworkObjectList()
+                    .filter { item ->
+                        item.imageId.isNullOrBlank().not()
+                    }
+                newList?.addAll(resultFilteredList)
+                artworks.value = newList
+            }
+        }
+    }
 }
