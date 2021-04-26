cbaf270c348ed960740e866aa35bf59d5ec29f5c
nikol412 <travin.nikita@gmail.com>
2021-04-26 17:20:54 +0600

    add navigation config in activity

--- app/src/main/java/com/nikol412/artroom/ui/MainActivity.kt
+++ app/src/main/java/com/nikol412/artroom/ui/MainActivity.kt
@@ -2,11 +2,26 @@ package com.nikol412.artroom.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavHostController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.nikol412.artroom.R
+import com.nikol412.artroom.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         setContentView(R.layout.activity_main)
+
+        val navController = (supportFragmentManager.findFragmentById(R.id.main_container_view) as NavHostFragment).navController
+
+        val appBarConf = AppBarConfiguration(navController.graph)
+        findViewById<Toolbar>(R.id.toolbar).setupWithNavController(navController, appBarConf)
     }
 }
